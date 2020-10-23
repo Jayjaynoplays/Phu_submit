e8dc52620f9114742442c5f9dd1ba4c5ef840b03
Jayjaynoplays <nhenh2712@gmail.com>
2020-10-24 00:10:43 +0700

    edit for more test cases of increase check

--- submit/increase_check.js
+++ submit/increase_check.js
@@ -5,18 +5,27 @@ var parsed = parseInt(number);
 
 function checkValid(){
 	if(isNaN(parsed)){
-		console.log("Invalid Input")
+		console.log("Invalid Input"); //check input có là phải chuỗi số hay ko
 		return false;
+	} else{
+		for (var i=0; i<=number.length-1; i++){  // check cho input có dạng: "123savcd"
+			if(number.charCodeAt(i)<48 || number.charCodeAt(i)>57){
+				console.log("Invalid Input");
+				return false;
+				break;
+			}
+		}
 	}
 	return true;
 }
 
 function check(){
+	//đặt ra các if để giảm cho vòng for
 	if (parsed % 10 == 0){
-		return false;       // chia het cho 10 la auto chuoi do khong tang, giam truong hop cho vong for
+		return false;       // chia hết cho 10 thì auto chuỗi đó không tăng
 	}
 	if (parsed<10){
-		return true;   // be hon 10 la auto chuoi do tang
+		return true;   // số đó bé hơn 10 là auto chuỗi đó tăng
 	}
 	for(var i=0; i<=number.length-1; i++){
 		if(number[i] > number[i+1]){  // nếu số trước bé hơn số sau thì về false rồi break lặp;
@@ -34,6 +43,8 @@ function main(){
  		}else{
  			console.log("nah, it's not increase!")
  		}
+ 	}else{
+
  	}
 }
 
